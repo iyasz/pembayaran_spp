97d9f3ba21ec77c9831b47511c86690767bb5b16
Yasz Avellia <zakamaragames@gmail.com>
2022-12-19 16:08:23 +0700

    menambah admin view

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.mainlayout')
+@section('title', 'Dashboard')
+@section('dsh', 'active')
+@section('icdsh', 'active-icon')
+    
+
+@section('content')
+<div class="row">
+    <div class="col-12">
+
+        <div class="height-100 main">
+            <h4>Your Dashboard</h4>
+         
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/mainlayout.blade.php
@@ -0,0 +1,70 @@
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Yasz - @yield('title')</title>
+
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"> -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- font awesome  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+
+    <!-- izitoast  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/css/iziToast.css">
+
+    <!-- boxicons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+
+    <!-- css style  -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+
+</head>
+
+
+<body id="body-pd">
+    <header class="header" id="header">
+        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+        <div class="header_img"> <img src="" alt=""> </div>
+    </header>
+    <div class="l-navbar" id="nav-bar">
+        <nav class="nav">
+            <div> <a href="" class="nav_logo"> <i class='bx bx-layer nav_logo-icon'></i> <span class="nav_logo-name">I'M !</span> </a>
+                <div class="nav_list">
+                    <a href="" class="nav_link @yield('dsh')"> <i class='bx bx-grid-alt @yield('icdsh') nav_icon'></i> <span class="nav_name">Dashboard</span> </a>
+                    <a href="admin/" class="nav_link @yield('adm')"> <i class='bx bx-user nav_icon'></i> <span class="nav_name">Admin</span>
+                    </a> <a href="barang/" class="nav_link @yield('swa')"><i class='bx bx-package nav_icon'></i> <span class="nav_name">Siswa</span> </a>
+                    <a href="transaksi/" class="nav_link"> <i class='bx bx-clipboard nav_icon'></i> <span class="nav_name">Transaksi</span>
+                    </a> <a href="stats/" class="nav_link"> <i class='bx bx-bar-chart-alt-2 nav_icon'></i> <span class="nav_name">Stats</span> </a>
+                </div>
+            </div>
+            <hr class="mx-3 hr-nav mb-0">
+            <div class="mb-5 mt-0">
+                <a href="" class="nav_link mb-2"><i class='bx bx-cog nav_icon'></i> <span class="nav_name">Settings</span></a>
+                <a href="logout.php" class="nav_link "><i class='bx bx-log-out nav_icon icon-bot'></i> <span class="nav_name name-bot">Log Out</span> </a>
+            </div>
+        </nav>
+    </div>
+    <!--Container Main start-->
+    <div class="container">
+        @yield('content')
+    </div>
+    <!--Container Main end-->
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/izitoast/1.4.0/js/iziToast.min.js" integrity="sha512-Zq9o+E00xhhR/7vJ49mxFNJ0KQw1E1TMWkPTxrWcnpfEFDEXgUiwJHIKit93EW/XxE31HSI5GEOW06G6BF1AtA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
